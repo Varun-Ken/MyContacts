72c79e3e7442a2ea0bd591b3ff52c4855b7c74ef
Varun-PBD <varunnurav1810@gmail.com>
2025-07-06 23:16:27 +0530

    dotenv config

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express")
-const dotenv = require("dotenv")
+const dotenv = require("dotenv").config()
 
-dotenv.config()
 const PORT = process.env.PORT || 3001
 
 const app = express()
