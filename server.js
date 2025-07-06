26aaacf92fe29c86bc786f0e1592ed587ba332f8
Varun-PBD <varunnurav1810@gmail.com>
2025-07-06 23:37:29 +0530

    feat : Router

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const dotenv = require("dotenv").config()
 
 const PORT = process.env.PORT || 3001
 
+const contactRouter = require("./routes/contactRoutes")
+
 const app = express()
+
+app.use("/api/contacts/",contactRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`);
